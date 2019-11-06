680617ab2d7dd1e5d4b76419f49d6addee35cd43
Lars <Iglioth@gmail.com>
2019-11-06 09:39:20 +0100

    Init Create Project

---
+++ Outsource Opdracht/Outsource Opdracht/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Outsource_Opdracht.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
